c4f5e4d7ae5bbd09768a9c17bc7c27e803a0a687
imkhateeb <luckykhateeb2@gmail.com>
2025-01-22 16:05:15 +0530

    [SET-UP]: Frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,6 @@
+import "./styles/animations.css";
+import "./styles/click-effects.css";
+
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
